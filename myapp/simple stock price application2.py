5f416b57a6b5ee52258030a7216bb8cf5ab3c968
Maram Sunil Reddy <67134688+MaramSunilReddy@users.noreply.github.com>
2021-11-28 19:57:29 +0530

    Update simple stock price application2.py
    apple stock

--- myapp/simple stock price application2.py
+++ myapp/simple stock price application2.py
@@ -1,1 +1,24 @@
+import yfinance as yf
+import streamlit as st
 
+st.write("""
+# Simple Stock Price App
+Shown are the stock **closing price** and ***volume*** of Apple!
+""")
+
+#define the ticker symbol
+tickerSymbol = 'AAPL'
+#get data on this ticker
+tickerData = yf.Ticker(tickerSymbol)
+#get the historical prices for this ticker
+tickerDf = tickerData.history(period='1d', start='2011-8-26', end='2021-11-25')
+# Open	High	Low	Close	Volume	Dividends	Stock Splits
+
+st.write("""
+## Closing Price
+""")
+st.line_chart(tickerDf.Close)
+st.write("""
+## Volume Price
+""")
+st.line_chart(tickerDf.Volume)

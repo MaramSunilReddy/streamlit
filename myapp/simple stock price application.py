fc8d2333ed63ac855fe61566724f0ca718be6476
Maram Sunil Reddy <67134688+MaramSunilReddy@users.noreply.github.com>
2021-11-28 19:18:47 +0530

    Create simple stock price application.py
    here we can get the historical prices of the google stock closing price and volume

---
+++ myapp/simple stock price application.py
@@ -0,0 +1,18 @@
+import yfinance as yf
+import streamlit as st
+
+st.write("""
+# Simple Stock Price App
+Shown are the stock closing price and volume of Google!
+""")
+
+#define the ticker symbol
+tickerSymbol = 'GOOGL'
+#get data on this ticker
+tickerData = yf.Ticker(tickerSymbol)
+#get the historical prices for this ticker
+tickerDf = tickerData.history(period='1d', start='2011-8-30', end='2021-11-26')
+# Open	High	Low	Close	Volume	Dividends	Stock Splits
+
+st.line_chart(tickerDf.Close)
+st.line_chart(tickerDf.Volume)
